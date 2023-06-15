f66ac9ffc07141fd9480fb9fcec08cc595e571dc
Rakshan0004 <rakshanch0004@gmail.com>
2023-06-15 10:26:58 +0530

    butterfly

--- patterns.java
+++ patterns.java
@@ -1,7 +1,7 @@
 //https://www.youtube.com/watch?v=lsOOs5J8ycw&t=1s
 public class patterns {
     public static void main(String[] args) {
-        pattern5(5);
+        inverted_half_pyramid(5);
     }
 
     static void pattern1(int n) {
@@ -37,6 +37,21 @@ public class patterns {
             int totalcolsinrow = row > n ? 2 * n - row - 1 : row;
             for (int col = 1; col <= totalcolsinrow; col++) {
                 System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
+
+    static void inverted_half_pyramid(int n) {
+        // rows
+        for (int i = 1; i <= n; i++) {
+            // for spaces
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+                // for columns
+            }
+            for (int z = 1; z <= i; z++) {
+                System.out.print("*");
             }
             System.out.println();
         }
