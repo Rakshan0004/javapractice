ba3845afe0f3a2ded074dd92d0b277f9467e37d9
Rakshan0004 <rakshanch0004@gmail.com>
2023-06-17 15:15:28 +0530

    linearsearch

--- patterns.java
+++ patterns.java
@@ -1,7 +1,7 @@
 //https://www.youtube.com/watch?v=lsOOs5J8ycw&t=1s
 public class patterns {
     public static void main(String[] args) {
-        inverted_half_pyramid(5);
+        butterfly(4);
     }
 
     static void pattern1(int n) {
@@ -51,6 +51,41 @@ public class patterns {
                 // for columns
             }
             for (int z = 1; z <= i; z++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void butterfly(int n) {
+        // first half
+        for (int i = 1; i <= n; i++) {
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // to print 2nd half of butterfly pattern
+        for (int i = n; i >= 1; i--) {
+            // same code as 1st half
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
