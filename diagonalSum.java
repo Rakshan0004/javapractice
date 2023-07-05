dd5bb6c89e1ce402f6fb739d1e313435041fa85e
Rakshan0004 <rakshanch0004@gmail.com>
2023-07-05 16:48:48 +0530

    diagonal sum using O(1)

--- diagonalSum.java
+++ diagonalSum.java
@@ -1,15 +1,21 @@
 public class diagonalSum {
     public static int diagonal(int matrix[][]) {
         int sum = 0;
-        // for primary diagonal
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (i == j) {
-                    sum += matrix[i][j];
-                } else if (i + j == matrix.length - 1) {// secondary diagonal
-                    sum += matrix[i][j];
-                }
-            }
+        // for primary diagonal //O(n^2)
+        // for (int i = 0; i < matrix.length; i++) {
+        // for (int j = 0; j < matrix[0].length; j++) {
+        // if (i == j) {
+        // sum += matrix[i][j];
+        // } else if (i + j == matrix.length - 1) {// secondary diagonal
+        // sum += matrix[i][j];
+        // }
+        // }
+        // }
+        for (int i = 0; i < matrix.length; i++) {// primary diagnal
+            sum += matrix[i][i];
+            if (i != matrix.length - 1 - i) { // this condition to make inot equal to j
+                sum += matrix[i][matrix.length - 1 - i]; // secondary diagonal
+            } // O(1)
         }
         return sum;
     }
