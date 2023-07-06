2ca8fbee0693322ae75a8d3fe16881421f3b3e4a
Rakshan0004 <rakshanch0004@gmail.com>
2023-07-06 13:45:51 +0530

    shortest path problem

--- stringpalindrome.java
+++ stringpalindrome.java
@@ -1,6 +1,6 @@
 public class stringpalindrome {
     public static boolean palindrome(String str) {
-        for (int i = 0; i < str.length() / 2; i++) {
+        for (int i = 0; i < str.length() / 2; i++) { // O(n)
             int n = str.length();
             if (str.charAt(i) == str.charAt(n - 1 - i)) {
 
@@ -11,8 +11,31 @@ public class stringpalindrome {
         return false;
     }
 
+    public static float getshortestpath(String path) {
+        int x = 0, y = 0;
+
+        for (int i = 0; i < path.length(); i++) {
+            char direction = path.charAt(i);
+            // south
+            if (direction == 'S') {
+                y--;
+            } else if (direction == 'N') {
+                y++;
+            } else if (direction == 'W') {
+                x--;
+            } else if (direction == 'E') {
+                x++;
+            }
+        }
+        int X2 = x * x; // (x-0)^2
+        int Y2 = y * y;
+        return (float) Math.sqrt(X2 + Y2);
+    }
+
     public static void main(String args[]) {
         String str = "racecar";
-        System.out.println(palindrome(str));
+        // System.out.println(palindrome(str));
+        String path = "WNEENESENNN";
+        System.out.print(getshortestpath(path));
     }
 }
