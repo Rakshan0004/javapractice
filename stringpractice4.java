a076ab04a0bf95c3a729a06259f67f6446b3b610
Rakshan0004 <rakshanch0004@gmail.com>
2023-07-07 19:20:06 +0530

    .

--- stringpractice4.java
+++ stringpractice4.java
@@ -1,22 +1,41 @@
 public class stringpractice4 {
     public static String Compression(String str) {
-        String newStr = "";
-        for (int i = 0; i < str.length(); i++) { // O(n)
-            Integer count = 1;
-            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+
+        /*
+         * String newStr = "";
+         * for (int i = 0; i < str.length(); i++) { // O(n)
+         * Integer count = 1;
+         * while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+         * count++;
+         * i++;
+         * }
+         * newStr += str.charAt(i);
+         * if (count > 1) {
+         * newStr += count.toString();
+         * }
+         * }
+         * return newStr;
+         * }
+         */
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
                 count++;
-                i++;
-            }
-            newStr += str.charAt(i);
-            if (count > 1) {
-                newStr += count.toString();
+            } else {
+                sb.append(str.charAt(i));
+                sb.append(count);
+                count = 1;
             }
         }
-        return newStr;
+        return sb.toString();
     }
 
     public static void main(String args[]) {
         String str = "aaabbcccdd";
+
         System.out.println(Compression(str));
+
     }
 }
