6e6a5eb21e80fd9c0b366a5ba75aec614eddd9fd
Rakshan0004 <rakshanch0004@gmail.com>
2023-07-07 13:07:53 +0530

    interview probelm

--- stringpalindrome.java
+++ stringpalindrome.java
@@ -55,7 +55,7 @@ public class stringpalindrome {
      */
     public static void main(String args[]) {
         StringBuilder sb = new StringBuilder("");
-        for (char ch = 'a'; ch <= z; ch++) {
+        for (char ch = 'a'; ch <= 'z'; ch++) {
             sb.append(ch);
         }
     }
